2427b07314f7b97de803ff9a8cd5b5992357cae8
acc1iela <acc1iela@gmail.com>
2020-09-29 00:39:52 +0900

    匿名関数前まで

--- javascript_smb/index.js
+++ javascript_smb/index.js
@@ -16,5 +16,67 @@ let worker = {
   plan() { },
   uploaded() { }
 };
-
 worker.list.business.creator.name();
+
+// f-2
+const foo = document.getElementById('foo');
+const fooWidth = foo.offsetWidth;
+const fooHeight = foo.offsetHeight;
+const fooArea = fooWidth * fooHeight;
+
+/*
+constの特徴
+再代入できない、再定義できない
+*/
+const obj = {
+  foo: 123
+};
+
+console.log(obj.foo);
+obj.foo = 456;
+console.log(obj.foo);
+
+/*
+letの特徴
+再代入できる、再定義できない
+ */
+let letNumber = 100;
+letNumber = 200;
+
+console.log('LGTM!');
+
+window.alert();
+
+window.console.log();
+
+window.document.getElementById('');
+
+const fool = {
+  // alert: function() {},
+  alert() { }, // 短縮記法
+};
+
+fool.alert();
+
+function twitterAPI(post) {
+  const endAPI = post.slice();
+  return endAPI;
+}
+
+const endTwitter = post(foodPost);
+const startTwitter = post(textPost);
+
+
+function isTweetable(text) {
+  return text.length <= 140;
+}
+
+console.log(isTweetable('なんか今日は非常に眠いです'));
+
+function alertTweetable(text) {
+  if (text.length <= 140) {
+    alert('you can tweet!');
+  }
+}
+
+alertTweetable('Looks Good to Me!!');
