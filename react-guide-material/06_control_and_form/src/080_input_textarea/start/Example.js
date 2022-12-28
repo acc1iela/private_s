485e10e27d11eb285fae929eb71f5522207bcedd
acc1iela <acc1iela@gmail.com>
2022-12-28 23:19:15 +0900

    update input_textareaの制御について

--- react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.js
+++ react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,10 +1,26 @@
 const Example = () => {
+  const [val, setVal] = useState('');
+  const clearVal = () => setVal('');
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <label htmlFor="456">ラベル</label>
+      <div>
+        <input
+          id="123"
+          placeholder="こんにちは"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <textarea
+          id="456"
+          placeholder="こんにちは"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+      </div>
+      <h3>{val}</h3>
+      <button Onclick={clearVal}>クリア</button>
+    </div>
   );
 };
 
