73d007bd253e4e17003baeef76936e9a78951106
acc1iela <acc1iela@gmail.com>
2023-01-08 04:16:46 +0900

    update: useState: 練習問題解答

--- react-guide-material/05_state_and_event/src/055_practice_state/start/Example.js
+++ react-guide-material/05_state_and_event/src/055_practice_state/start/Example.js
@@ -1,16 +1,15 @@
-import { useState } from 'react';
-
 const Example = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
   const countUp = () => {
-    setCount((state) => state + 1);
+    setCount(count + 1);
+    setCount((prevstate) => prevstate + 1);
   };
   const countDown = () => {
-    setCount((state) => state + 1);
+    setCount(count - 1);
   };
   return (
     <>
-      <p>現在のカウント数： {count}</p>
+      <p>現在のカウント数: {count}</p>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
