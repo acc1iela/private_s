b076d42bce8ce8ba2770bd039c3a503f8d04290d
acc1iela <acc1iela@gmail.com>
2023-01-09 02:28:58 +0900

    update: state_arrayについて

--- react-guide-material/05_state_and_event/src/064_state_array/start/Example.js
+++ react-guide-material/05_state_and_event/src/064_state_array/start/Example.js
@@ -3,18 +3,18 @@ import { useState } from 'react';
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
   const [nums, setNums] = useState(numArray);
-  console.log(nums);
-  const shuffle = () => {
-    const newNums = [...nums];
+
+  cosnt shuffle = () => {
+    const newNums = [...nums ];
     const value = newNums.pop();
     newNums.unshift(value);
     setNums(newNums);
-    console.log(nums === newNums); //異なる値
-  };
+  }
+
   return (
     <>
       <h1>{nums}</h1>
-      <button onClick={shuffle}></button>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
