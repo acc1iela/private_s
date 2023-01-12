74d63f51107dc370c3f53b49e90bf72eba9255a5
acc1iela <acc1iela@gmail.com>
2023-01-13 02:20:01 +0900

    update: gitignore追記

--- react-guide-material/06_control_and_form/src/130_reminder/start/components/Form.js
+++ react-guide-material/06_control_and_form/src/130_reminder/start/components/Form.js
@@ -1,25 +1,21 @@
 import { useState } from 'react';
+
 const Form = ({ createTodo }) => {
   const [enteredTodo, setEnteredTodo] = useState('');
-
   const addTodo = (e) => {
     e.preventDefault();
-
     const newTodo = {
       id: Math.floor(Math.random() * 1e5),
       content: enteredTodo,
     };
 
     createTodo(newTodo);
-
-    setEnteredTodo('');
   };
   return (
     <div>
-      <form onSubmit={addTodo}>
-        <input type="text" value={enteredTodo} onChange={(e) => setEnteredTodo(e.target.value)} />
-        <button>追加</button>
-      </form>
+      <input type="text" value={enteredTodo} onChange={(e) => setEnteredTodo(e.target.value)} />
+      <button onClick={addTodo}>追加</button>
+      <span>{enteredTodo}</span>
     </div>
   );
 };
