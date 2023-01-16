5b99c441580f4328a33c24ddc030528dde1bca00
acc1iela <acc1iela@gmail.com>
2023-01-17 05:50:45 +0900

    update: use refについて

--- react-guide-material/08_other_function/src/030_useRef/start/Example.js
+++ react-guide-material/08_other_function/src/030_useRef/start/Example.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import { useState, useRef } from 'react';
 
 const Case1 = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
+  const inputRef = useRef();
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
-        インプット要素をフォーカスする
-      </button>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>インプット要素をフォーカスする</button>
     </div>
   );
 };
