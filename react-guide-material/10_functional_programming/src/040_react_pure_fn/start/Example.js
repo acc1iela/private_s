3779b47e9f9d692827948992affacd7bb84df922
acc1iela <acc1iela@gmail.com>
2023-01-18 01:02:15 +0900

    update: react_pure_fn

--- react-guide-material/10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ react-guide-material/10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,19 @@
-const Example = () => {
+let value = 0;
 
+const Child = () => {
+  value++;
+  return <div>{value}</div>;
+};
+
+const ChildPure = ({ value }) => {
+  return <div>{value}</div>;
+};
+
+const Example = () => {
+  let value = 0;
   return (
     <>
+      <ChildPure value={++value} />
     </>
   );
 };
