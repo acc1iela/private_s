0c22c42ced8d2a1336aa7b778c3f5661297a7cfb
acc1iela <acc1iela@gmail.com>
2023-01-18 00:45:18 +0900

    update: pure functionについて

--- react-guide-material/10_functional_programming/src/020_pure_function/start/Example.js
+++ react-guide-material/10_functional_programming/src/020_pure_function/start/Example.js
@@ -1,8 +1,17 @@
 const Example = () => {
+  const val1 = 3,
+    val2 = 4;
+
+  let result;
+  const add = (val1, val2) => {
+    result = val1 + val2;
+    return val1 + val2;
+  };
   return (
     <>
       <h3>純粋関数</h3>
       <p>fn(決まった引数) には 決まった戻り値 を返す</p>
+      <div>純粋関数：{addEventListener(val1, val2)}</div>
     </>
   );
 };
