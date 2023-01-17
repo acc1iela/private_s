20810262f96e152b62578610bf2299f821e5e17d
acc1iela <acc1iela@gmail.com>
2023-01-18 00:37:25 +0900

    update: date_procedure

--- react-guide-material/10_functional_programming/src/010_data_procedure/start/Example.js
+++ react-guide-material/10_functional_programming/src/010_data_procedure/start/Example.js
@@ -1,8 +1,23 @@
 const Example = () => {
+  const numObj = {
+    nums: [1, 2, 3],
+    sum() {
+      const nums = this.nums;
+      let sumValue = 0;
+      for (let i = 0; i < nums.length; i++) {
+        sumValue += nums[i];
+      }
+      return sumValue;
+    },
+  };
+
+  const nums = [1, 2, 3];
+  const sum = (arry) => arry.reduce((accu, curr) => accu + curr);
   return (
     <>
       <h3>状態管理と処理を分離</h3>
       <p>状態（データ）と処理（やりたいこと）は切り離す</p>
+      <div>オブジェクト指向型:{numObj.sum()}</div>
     </>
   );
 };
