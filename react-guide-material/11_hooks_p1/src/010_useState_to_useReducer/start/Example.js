580389d2895a73284762da7a1e7eaa08e301b587
acc1iela <acc1iela@gmail.com>
2023-01-19 01:45:35 +0900

    update: useStateについて

--- react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if (action === '+') {
+      return ++prev;
+    } else if (action === '-') {
+      return --prev;
+    }
+  });
+
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+
+  const rcountUp = () => {
+    dispatch('+');
+  };
+
+  const rcountDown = () => {
+    dispatch('-');
+  };
+
   return (
     <>
+      <h3>{state}</h3>
+      <button onClick={countUp}>+</button>
+      <h3>{rstate}</h3>
+      <button onClick={rcountUp}>+</button>
+      <button onClick={rcountDown}>-</button>
     </>
   );
 };
