cbbe4b7c464e85a71c536e6510ef115dfb952c0e
acc1iela <acc1iela@gmail.com>
2023-01-20 22:50:43 +0900

    update: useReducer課題　

--- react-guide-material/11_hooks_p1/src/025_practice_useReducer/start/Example.js
+++ react-guide-material/11_hooks_p1/src/025_practice_useReducer/start/Example.js
@@ -1,8 +1,23 @@
-import { useReducer } from "react";
+import { useReducer } from 'react';
 
-const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
+const CALC_OPTIONS = ['add', 'minus', 'divide', 'multiply'];
 
-const reducer = () => {}
+const reducer = (state, { type, payload }) => {
+  switch (action.type) {
+    case 'change':
+      return { ...state, [payload.name]: payload.value };
+    case 'add':
+      return { ...state, result: state.a + state.b };
+    case 'minus':
+      return { ...state, result: state.a - state.b };
+    case 'multiply':
+      return { ...state, result: state.a * state.b };
+    case 'divide':
+      return { ...state, result: state.a / state.b };
+    default:
+      throw new Error('不明な値です');
+  }
+};
 
 const Example = () => {
   const initState = {
@@ -14,38 +29,33 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-    
+    dispatch({ type: e.target.value });
   };
 
   const numChangeHandler = (e) => {
-    
-  }
+    dispatch({ type: 'change', payload: { name: e.target.name, value: parseInt(e.targe.value) } });
+  };
 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
+      <div>
         a:
-        <input
-          type="number"
-          name="a"
-          value={state.a}
-          onChange={numChangeHandler}
-        />
+        <input type="number" name="a" value={state.a} onChange={numChangeHandler} />
       </div>
       <div>
         b:
-        <input
-          type="number"
-          name="b"
-          value={state.b}
-          onChange={numChangeHandler}
-        />
+        <input type="number" name="b" value={state.b} onChange={numChangeHandler} />
       </div>
       <select value={state.type} onChange={calculate}>
+        {CALC_OPTIONS.map((type) => {
+          return (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          );
+        })}
       </select>
-      <h1>結果：{state.result}</h1> */}
+      <h1>結果：{state.result}</h1>
     </>
   );
 };
