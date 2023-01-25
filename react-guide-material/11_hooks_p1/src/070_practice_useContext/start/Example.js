face4c50c4bd71eeee8fede5343d936e44ed79cb
acc1iela <acc1iela@gmail.com>
2023-01-25 23:33:43 +0900

    update: lint当てる

--- react-guide-material/11_hooks_p1/src/070_practice_useContext/start/Example.js
+++ react-guide-material/11_hooks_p1/src/070_practice_useContext/start/Example.js
@@ -1,27 +1,27 @@
-import { useReducer } from "react";
+import { useReducer } from 'react';
 
-const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
+const CALC_OPTIONS = ['add', 'minus', 'divide', 'multiply'];
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
-    case "change": {
+    case 'change': {
       const { name, value } = payload;
       return { ...state, [name]: value };
     }
-    case "add": {
+    case 'add': {
       return { ...state, result: state.a + state.b };
     }
-    case "minus": {
+    case 'minus': {
       return { ...state, result: state.a - state.b };
     }
-    case "divide": {
+    case 'divide': {
       return { ...state, result: state.a / state.b };
     }
-    case "multiply": {
+    case 'multiply': {
       return { ...state, result: state.a * state.b };
     }
     default:
-      throw new Error("operator is invalid");
+      throw new Error('operator is invalid');
   }
 };
 
@@ -35,39 +35,31 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-    dispatch({type: e.target.value});
+    dispatch({ type: e.target.value });
   };
   const numChangeHandler = (e) => {
-    dispatch({type: 'change', payload: {name: e.target.name, value: e.target.value}});
+    dispatch({ type: 'change', payload: { name: e.target.name, value: e.target.value } });
   };
   return (
     /* 完成系のJSX */
     // <CalcProvider>
-    //   <Input name="a"/>      
-    //   <Input name="b" />      
+    //   <Input name="a"/>
+    //   <Input name="b" />
     //   <Select />
-    //   <Result />      
+    //   <Result />
     // </CalcProvider>
     <>
       <h3>練習問題</h3>
-      <p>Example内のコードをコンポーネントに分割してください。また、ステートはContext経由でやり取りしてください。</p>
+      <p>
+        Example内のコードをコンポーネントに分割してください。また、ステートはContext経由でやり取りしてください。
+      </p>
       <div>
         a:
-        <input
-          type="number"
-          name="a"
-          value={state.a}
-          onChange={numChangeHandler}
-        />
+        <input type="number" name="a" value={state.a} onChange={numChangeHandler} />
       </div>
       <div>
         b:
-        <input
-          type="number"
-          name="b"
-          value={state.b}
-          onChange={numChangeHandler}
-        />
+        <input type="number" name="b" value={state.b} onChange={numChangeHandler} />
       </div>
       <select value={state.type} name="type" onChange={calculate}>
         {CALC_OPTIONS.map((type) => (
