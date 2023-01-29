597295580e37e09fc1750e45ce5931a6637b4ca3
acc1iela <acc1iela@gmail.com>
2023-01-29 23:14:16 +0900

    update: context作成 Exampleで読み込み

--- react-guide-material/11_hooks_p1/src/070_practice_useContext/start/Example.js
+++ react-guide-material/11_hooks_p1/src/070_practice_useContext/start/Example.js
@@ -2,39 +2,9 @@ import { useReducer } from 'react';
 import Input from './components/Input';
 import Select from './components/Select';
 import Result from './components/Result';
-
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case 'change': {
-      const { name, value } = payload;
-      return { ...state, [name]: value };
-    }
-    case 'add': {
-      return { ...state, result: state.a + state.b };
-    }
-    case 'minus': {
-      return { ...state, result: state.a - state.b };
-    }
-    case 'divide': {
-      return { ...state, result: state.a / state.b };
-    }
-    case 'multiply': {
-      return { ...state, result: state.a * state.b };
-    }
-    default:
-      throw new Error('operator is invalid');
-  }
-};
+import CalcProvider from './context/CalcContext';
 
 const Example = () => {
-  const initState = {
-    a: 1,
-    b: 2,
-    result: 3,
-  };
-
-  const [state, dispatch] = useReducer(reducer, initState);
-
   return (
     /* 完成系のJSX */
     // <CalcProvider>
@@ -44,14 +14,12 @@ const Example = () => {
     //   <Result />
     // </CalcProvider>
     <>
-      <h3>練習問題</h3>
-      <p>
-        Example内のコードをコンポーネントに分割してください。また、ステートはContext経由でやり取りしてください。
-      </p>
-      <Input name="a" />
-      <Input name="b" />
-      <Select />
-      <Result />
+      <CalcProvider>
+        <Input name="a" />
+        <Input name="b" />
+        <Select />
+        <Result />
+      </CalcProvider>
     </>
   );
 };
