4e0bbc397e0a1b00ac581e610956ee9041a9f4d2
acc1iela <acc1iela@gmail.com>
2023-01-29 23:05:47 +0900

    update:  残りもcomponentsに分割

---
+++ react-guide-material/11_hooks_p1/src/070_practice_useContext/start/components/Result.js
@@ -0,0 +1,5 @@
+const Result = () => {
+  return <h3>結果：{state.result}</h3>;
+};
+
+export default Result;
---
+++ react-guide-material/11_hooks_p1/src/070_practice_useContext/start/components/Select.js
@@ -0,0 +1,19 @@
+const CALC_OPTIONS = ['add', 'minus', 'divide', 'multiply'];
+
+const Select = () => {
+  const calculate = (e) => {
+    dispatch({ type: e.target.value });
+  };
+
+  return (
+    <select value={state.type} name="type" onChange={calculate}>
+      {CALC_OPTIONS.map((type) => (
+        <option key={type} value={type}>
+          {type}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
