868c1330fc23a0769683aba5493ea3e0ea8f774c
acc1iela <acc1iela@gmail.com>
2023-01-30 23:29:37 +0900

    update; components分割

--- react-guide-material/11_hooks_p1/src/080_practice_reminder/start/components/List.js
+++ react-guide-material/11_hooks_p1/src/080_practice_reminder/start/components/List.js
@@ -1,19 +1,16 @@
-const List = ({todos, deleteTodo}) => {
-    const complete = (id) => {
-        deleteTodo(id)
-    }
-    return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
+import item from './Item';
+
+const List = ({ todos, deleteTodo }) => {
+  const complete = (id) => {
+    deleteTodo(id);
+  };
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Item todo={todo} comlete={complete} key={todo.id} />
+      ))}
+    </div>
+  );
+};
 
 export default List;
