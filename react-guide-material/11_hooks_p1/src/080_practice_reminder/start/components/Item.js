1710fc528ae7fd4e13a946df64e3d72aafc69f50
acc1iela <acc1iela@gmail.com>
2023-01-31 23:39:21 +0900

    update: useState読み込み

--- react-guide-material/11_hooks_p1/src/080_practice_reminder/start/components/Item.js
+++ react-guide-material/11_hooks_p1/src/080_practice_reminder/start/components/Item.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 const Item = ({ todo, complete }) => {
+  const [editingContent, setEditingContent] = useState(todo.content);
   return (
     <div key={todo.id}>
       <button onClick={() => complete(todo.id)}>完了</button>
