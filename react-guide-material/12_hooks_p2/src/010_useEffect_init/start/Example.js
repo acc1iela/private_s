e76800c84751224e62444fd3d84633366d1a90b5
acc1iela <acc1iela@gmail.com>
2023-02-08 23:22:31 +0900

    update: useState

--- react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.js
+++ react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.js
@@ -1,5 +1,15 @@
+import { useState } from 'react';
+
 const Example = () => {
-  return <></>;
+  const [time, setTime] = useState(0);
+  return (
+    <>
+      <h3>
+        <time>{time}</time>
+        <span>秒経過</span>
+      </h3>
+    </>
+  );
 };
 
 export default Example;
