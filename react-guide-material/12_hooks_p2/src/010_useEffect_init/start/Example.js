1b2004c33aa0a039d2fb5906922f47e30153b2cd
acc1iela <acc1iela@gmail.com>
2023-02-08 23:26:04 +0900

    update: カウントダウン実装

--- react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.js
+++ react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.js
@@ -2,6 +2,12 @@ import { useState } from 'react';
 
 const Example = () => {
   const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    window.setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+  }, []);
   return (
     <>
       <h3>
