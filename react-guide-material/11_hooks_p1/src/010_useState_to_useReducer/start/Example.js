bccb3236559677c15efc7dd76c43ef2b0b14c6e3
acc1iela <acc1iela@gmail.com>
2023-02-21 21:51:30 +0900

    update: reducer復讐

--- react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -2,35 +2,17 @@ import { useState } from 'react';
 
 const Example = () => {
   const [state, setState] = useState(0);
-  const [rstate, dispatch] = useReducer((prev, action) => {
-    if (action === '+') {
-      return ++prev;
-    } else if (action === '-') {
-      return --prev;
-    }
-  });
 
   const countUp = () => {
-    setState((prev) => ++prev);
-  };
-
-  const rcountUp = () => {
-    dispatch('+');
-  };
-
-  const rcountDown = () => {
-    dispatch('-');
+    setState((prev) => {
+      return (prev) => ++prev;
+    });
   };
 
   return (
     <>
       <h3>{state}</h3>
-      <button onClick={countUp}>+</button>
-      <h3>{rstate}</h3>
-      <button onClick={rcountUp}>+</button>
-      <button onClick={rcountDown}>-</button>
+      <button onClick={countUp}></button>
     </>
   );
 };
-
-export default Example;
